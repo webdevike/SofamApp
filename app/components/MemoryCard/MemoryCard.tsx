4390f96c22bd32befeaa676b41436fc7f42b7e6b
ikey2244 <ikey2244@gmail.com>
2021-01-02 17:11:33 -0600

    Fix profile pciture

--- app/components/MemoryCard/MemoryCard.tsx
+++ app/components/MemoryCard/MemoryCard.tsx
@@ -16,7 +16,7 @@ export const MemoryCard: Component<MemoryCardProps> = ({ data }) => {
     <View style={styles.WRAPPER}>
       <Text style={styles.TITLE}>{data.title}</Text>
       <Text style={styles.DESCRIPTION}>{data.description}</Text>
-      <Image style={styles.PROFILE_IMAGE} source={{ uri: user?.me.profilePicture || 'https://medgoldresources.com/wp-content/uploads/2018/02/avatar-placeholder.gif' }} defaultSource={ require('../../../assets/loading.png')} />
+      <Image style={styles.PROFILE_IMAGE} source={{ uri: user?.me?.profilePicture || 'https://images.unsplash.com/photo-1529405643518-5cf24fddfc0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80' }} />
     </View>
   )
 }
